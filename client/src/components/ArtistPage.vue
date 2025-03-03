42bcb4b45e71c2cb653868e0be4bba268f7d9aed
Chantelle Cabanilla <chanillie11@gmail.com>
2025-03-02 16:25:27 -0800

    Added ARIA Landmarks to Artist Page

--- client/src/components/ArtistPage.vue
+++ client/src/components/ArtistPage.vue
@@ -1,50 +1,63 @@
 <!-- This file was written by Matthew Stenvold -->
+<!--ARIA Landmakes added by Chantelle Cabanilla-->
 <template>
-  <Navbar />
+  <header role="navigation">
+    <Navbar />
+  </header>
 
+  <main role = "main">
   <v-container v-if="data">
     <v-row>
       <v-col cols="12" md="3">
+        <section role="region" aria-label="Artist photo and basic info">
         <v-card class="pa-4 d-flex flex-column align-center">
           <v-img
             src="/images/UnknownPerson.png"
             width="200"
             height="200"
             contain
+            alt="Profile photo of artist"
           />
           <p class="mt-2">Born: December 13, 1989</p>
           <p>Genre: Pop, Country</p>
           <p>Aliases: {{ data.aliases?.join(', ') || 'None' }}</p>
         </v-card>
+        </section>
       </v-col>
 
       <v-col cols="12" md="9">
-        <h1 class="text-h4 font-weight-bold">{{ data.artist?.[0]?.name }}</h1>
+        <section role="region" aria-labelledby="artist-name">
+        <h1 id="artist-name" class="text-h4 font-weight-bold">{{ data.artist?.[0]?.name }}</h1>
         <RatingSystem 
           itemType="artist"
           :itemId="$route.params.artist_id"
         />
-
         <p class="mt-4">{{ data.artist?.[0]?.profile || 'No profile available' }}</p>
+      </section>
       </v-col>
     </v-row>
 
-    <div v-if="data.discography && data.discography.length">
-      <h2 class="text-h5 mt-6">Discography</h2>
+    <div role="region" aria-labelledby="discography-heading" v-if="data.discography && data.discography.length">
+      <h2 id="discography-heading" class="text-h5 mt-6">Discography</h2>
 
       <v-row class="align-center mb-4">
         <v-col cols="12" md="4">
+          <div role="search" aria-label="Sort discography">
+              <label for="sort-select" class="sr-only">Sort discography by</label>
           <v-select
+             id="sort-select"
             v-model="sortBy"
             :items="['Popularity', 'Release Date']"
             label="Sort By"
             outlined
             dense
+            aria-label="Sort discography by"
           />
+          </div>
         </v-col>
       </v-row>
 
-      <v-row>
+      <v-row aria-label="Albums List">
         <v-col
           v-for="(release, index) in data.discography"
           :key="index"
@@ -52,17 +65,21 @@
           sm="4"
           md="3"
         >
+        <article aria-labelledby="album-title-{{index}}">
           <v-card class="pa-2" outlined>
-            <v-img src="/images/UnknownSong.png" height="120" contain />
+            <v-img src="/images/UnknownSong.png" height="120" contain :alt="`Album cover`"/>
             <v-card-title>
-              <router-link :key="i" :to="`/master/${release.master_id}`">{{ release.title }}</router-link>
+              <router-link :key="i" :to="`/master/${release.master_id}`":aria-label="`View details for album`"></router-link>
             </v-card-title>
           </v-card>
+          </article>
         </v-col>
       </v-row>
     </div>
   </v-container>
-  <div v-else class="loading">Loading...</div>
+
+  <div v-else class="loading" role="status" aria-live="polite">Loading...</div>
+  </main>
 </template>
 
 <script>
@@ -123,6 +140,19 @@ export default {
 .v-card-title {
   font-size: 0.9rem;
   font-weight: bold;
+}
+
+/* For screen readers only - visually hidden but accessible to screen readers */
+.sr-only {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border-width: 0;
 }
 
 </style>
